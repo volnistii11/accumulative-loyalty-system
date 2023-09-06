dae66445ed5006fda7e9af6300c9a16e3addbc65
volnistii11 <cor.fascio@gmail.com>
2023-09-07 00:54:54 +0600

    Add gracefull shutdown

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"github.com/volnistii11/accumulative-loyalty-system/internal/app/gophermart/client"
 	"github.com/volnistii11/accumulative-loyalty-system/internal/app/gophermart/server"
 	"github.com/volnistii11/accumulative-loyalty-system/internal/config"
@@ -10,6 +11,9 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
@@ -49,7 +53,33 @@ func main() {
 	go client.DoAccrualIfPossible(logger, storage, cfg)
 
 	router := server.NewRouter(logger, storage, cfg).Serve()
-	http.ListenAndServe(cfg.GetHTTPServerAddress(), router)
 
-	// TODO: swagger
+	server := &http.Server{Addr: cfg.GetHTTPServerAddress(), Handler: router}
+	serverCtx, serverStopCtx := context.WithCancel(context.Background())
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	go func() {
+		<-sig
+		shutdownCtx, _ := context.WithTimeout(serverCtx, 30*time.Second)
+
+		go func() {
+			<-shutdownCtx.Done()
+			if shutdownCtx.Err() == context.DeadlineExceeded {
+				log.Fatal("graceful shutdown timed out.. forcing exit.")
+			}
+		}()
+
+		err := server.Shutdown(shutdownCtx)
+		if err != nil {
+			log.Fatal(err)
+		}
+		serverStopCtx()
+	}()
+
+	err = server.ListenAndServe()
+	if err != nil && err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
+	<-serverCtx.Done()
 }
