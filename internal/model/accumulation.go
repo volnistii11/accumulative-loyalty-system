99f2999b6c628c3cd921c9b1f6ca66b1732b3d19
volnistii11 <cor.fascio@gmail.com>
2023-09-07 01:22:52 +0600

    Fix minor err

--- internal/model/accumulation.go
+++ internal/model/accumulation.go
@@ -5,7 +5,7 @@ import "time"
 type Accumulation struct {
 	ID               int        `json:"id,omitempty" gorm:"primaryKey"`
 	UserID           int        `json:"user_id,omitempty"`
-	OrderNumber      string     `json:"number,omitempty,string"`
+	OrderNumber      string     `json:"number,omitempty"`
 	UploadedAt       *time.Time `json:"uploaded_at,omitempty"`
 	ProcessingStatus string     `json:"status,omitempty"`
 	AccrualStatus    string     `json:"accrual_status,omitempty"`
@@ -21,12 +21,12 @@ type Balance struct {
 }
 
 type Withdraw struct {
-	OrderNumber    string  `json:"order,string"`
+	OrderNumber    string  `json:"order"`
 	WriteOffAmount float64 `json:"sum"`
 }
 
 type Withdrawal struct {
-	OrderNumber string     `json:"order,string"`
+	OrderNumber string     `json:"order"`
 	Amount      float64    `json:"sum"`
 	ProcessedAt *time.Time `json:"processed_at"`
 }
@@ -34,7 +34,7 @@ type Withdrawal struct {
 type Withdrawals []*Withdrawal
 
 type AccrualSystemAnswer struct {
-	OrderNumber   string  `json:"order,string"`
+	OrderNumber   string  `json:"order"`
 	AccrualStatus string  `json:"status"`
 	Amount        float64 `json:"accrual"`
 }
